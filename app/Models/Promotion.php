104a5eded7ece4269a7b4fc4da530cae6b282516
mr-leo44 <lionnelkaniki44@gmail.com>
2024-11-15 09:13:28 +0100

    add some relation to promotion model

--- app/Models/Promotion.php
+++ app/Models/Promotion.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 use App\Models\Faculty;
+use App\Models\Student;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Promotion extends Model
@@ -28,4 +30,10 @@ class Promotion extends Model
     {
         return $this->belongsTo(Faculty::class);
     }
+
+    public function students(): HasMany
+    {
+        return $this->hasMany(Student::class);
+    }
+
 }
