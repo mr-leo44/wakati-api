dddbdec8c5cf5e33ffc424258bebf04a5c0abc05
mr-leo44 <lionnelkaniki44@gmail.com>
2024-11-15 09:13:54 +0100

    add some relations to university model

--- app/Models/University.php
+++ app/Models/University.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Admin;
 use App\Models\Faculty;
+use App\Models\Student;
+use App\Models\AdminFac;
+use App\Models\Professor;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class University extends Model
@@ -35,4 +40,26 @@ class University extends Model
     {
         return $this->belongsToMany(Faculty::class);
     }
+
+    public function professors(): BelongsToMany
+    {
+        return $this->belongsToMany(Professor::class);
+    }
+
+    public function adminFacs(): HasMany
+    {
+        return $this->hasMany(AdminFac::class);
+    }
+
+    public function admins(): HasMany
+    {
+        return $this->hasMany(Admin::class);
+    }
+
+    
+    public function students(): HasMany
+    {
+        return $this->hasMany(Student::class);
+    }
+
 }
