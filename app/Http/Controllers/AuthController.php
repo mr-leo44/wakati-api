3120fc20773034c1f71e9e186464665e07bea034
mr-leo44 <lionnelkaniki44@gmail.com>
2024-11-15 09:53:26 +0100

    add login method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function login(LoginRequest $request) 
+    {
+        try {
+            $fieldType = filter_var($request->login, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+            $user = User::with('account')->where($fieldType, $request->login)->first();
+            if ($user === null || !Hash::check($request->password, $user->password)) {
+                return response()->json(['error' => 'Identifiants incorrects'], 401);
+            } else {
+                $token = $user->createToken($user->email)->plainTextToken;
+                $type_user = $user->account->accountables->getMorphClass();
+                $type_user = class_basename($type_user);
+
+                return response()->json([
+                    'user' => $user,
+                    'tooken' => $token,
+                    'type_user' => $type_user,
+                ]);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Erreur interne de serveur'], 500);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
