c64787ed09e788f91597b1be157f17db11bbdf75
mr-leo44 <lionnelkaniki44@gmail.com>
2024-11-21 00:46:35 +0100

    update activation route

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
-    Route::get('/activate', [AuthController::class, 'activate'])->name('student.activate');
+    Route::post('/activate', [AuthController::class, 'activate'])->name('student.activate');
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
 });
 
