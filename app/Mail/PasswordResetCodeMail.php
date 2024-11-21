dfc3d84aeebe8e2499cecbbeddf69bac2dc687d6
mr-leo44 <lionnelkaniki44@gmail.com>
2024-11-21 02:38:21 +0100

    add password reset mailable

---
+++ app/Mail/PasswordResetCodeMail.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class PasswordResetCodeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(public User $user, public int $resetCode){}
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Réinitialisation de votre mot de passe wakati-app',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mails.password-reset',
+            with: [
+                'user' => $this->user,
+                'resetCode' => $this->resetCode,
+            ]
+        );
+    }
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
