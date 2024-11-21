4ff34b4bab6da40e1b937e0d0943d40330ed1794
mr-leo44 <lionnelkaniki44@gmail.com>
2024-11-21 01:50:23 +0100

    update login method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,26 +18,16 @@ class AuthController extends Controller
 
     public function __construct(public AccountService $accountService) {}
 
-    public function login(LoginRequest $request) : JsonResponse
+    public function login(LoginRequest $request): JsonResponse
     {
+        $validated = $request->validated();
         try {
-            $fieldType = filter_var($request->login, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
-            $user = User::with('account')->where($fieldType, $request->login)->first();
-            if ($user === null || !Hash::check($request->password, $user->password)) {
-                return response()->json(['error' => 'Identifiants incorrects'], 401);
-            } else {
-                $token = $user->createToken($user->email)->plainTextToken;
-                $type_user = $user->account->accountables->getMorphClass();
-                $type_user = class_basename($type_user);
-
-                Auth::login($user, true);
-
-                return response()->json([
-                    'user' => $user,
-                    'token' => $token,
-                    'type' => $type_user,
-                ]);
-            }
+            $result = $this->accountService->loginUser($validated);
+            return response()->json([
+                'user' => $result['user'],
+                'token' => $result['token'],
+                'type' => $result['type_user'],
+            ]);
         } catch (\Exception $e) {
             return response()->json(['error' => 'Erreur interne de serveur'], 500);
         }
@@ -52,10 +42,10 @@ class AuthController extends Controller
         ]);
 
         $result = $this->accountService->createUser(array_merge($validated, ['accountable_type' => Student::class, 'accountable_id' => $student->id]));
-            return response()->json([
-                'message' => 'Compte créé avec succès',
-                'user' => $result,
-            ], 201);
+        return response()->json([
+            'message' => 'Compte créé avec succès',
+            'user' => $result,
+        ], 201);
     }
 
     public function activate(Request $request)
