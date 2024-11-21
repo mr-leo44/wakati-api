2680decfbb1b6e9cf35d1be9dc7f09247f4f2a11
mr-leo44 <lionnelkaniki44@gmail.com>
2024-11-21 03:17:24 +0100

    add reset password routes

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,8 @@ Route::group(['prefix' => 'auth'], function () {
     Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/activate', [AuthController::class, 'activate'])->name('student.activate');
     Route::get('/activate/{token}', [UserController::class, 'activate'])->name('user.activate');
+    Route::post('/password/forgot', [AuthController::class, 'sendResetCode'])->name('password.sendReset');
+    Route::post('/password/verify', [AuthController::class, 'verifyResetCode'])->name('password.verify');
+    Route::post('/password/reset', [AuthController::class, 'resetNewPassword'])->name('password.reset');
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
 });
-
