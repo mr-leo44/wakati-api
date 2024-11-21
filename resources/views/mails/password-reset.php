a025cb81e603695544fa2ce6bc99800082a77b50
mr-leo44 <lionnelkaniki44@gmail.com>
2024-11-21 02:38:35 +0100

    add password-reset mail

---
+++ resources/views/mails/password-reset.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Réinitialisation de mot de passe</title>
+</head>
+
+<body>
+    <p>Bonjour {{ $user->name }},</p>
+    <br>
+    <p>Voici le code réinitialisation de votre mot de passe !</p>
+    <h3><b>{{ $resetCode }}</b></h3>
+    </p>
+    <p>Ce code est valable que pendant <strong>5 minutes</strong></p>
+    <p>Si vous n'avez pas demandé de code veuillez ignorer ce message</p>
+    <br>
+    <p>Cordialement!</p>
+    <p>Support {{ config('app.name') }}</p>
+</body>
+
+</html>
