1ad3bf53a89ff51231017cdfe800b063a7883983
mr-leo44 <lionnelkaniki44@gmail.com>
2024-11-21 01:31:10 +0100

    update register request

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -25,13 +25,12 @@ class RegisterRequest extends FormRequest
             'name' => 'required|string|max:191|min:3',
             'email' => 'required|email|string|lowercase|max:191|unique:users,email',
             'username' => 'required|string|min:3|max:191|unique:users,username',
-            // 'avatar' => 'nullable|file|mimes:jpg,png,jpeg|max:300',
             'phone' => 'required|string|unique:users,phone',
             'password' => 'required_if:type,student|string|min:8|max:15|regex:/[A-Z]/|regex:/[@$!%*#?&]/',
             'university_id' => 'required_if:type,student,admin,admin_fac,professor|exists:universities,id',
             'faculty_id' => 'required_if:type,student,admin_fac|exists:faculties,id',
             'promotion_id' => 'required_if:type,student|exists:promotions,id',
-            'type' => 'required|string|in:student,admin,admin_fac,professor,SuperAdmin',
+            'type' => 'required|string|in:student,admin,admin_fac,professor,superAdmin',
         ];
     }
 
