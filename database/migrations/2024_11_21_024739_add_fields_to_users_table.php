77d232ee54a7c1c815e3e44d2ce682fd69b292c4
mr-leo44 <lionnelkaniki44@gmail.com>
2024-11-21 02:58:01 +0100

    add columns to users table

---
+++ database/migrations/2024_11_21_024739_add_fields_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('reset_code')->after('activation_token')->nullable();
+            $table->timestamp('reset_code_expires_at')->after('reset_code')->nullable();
+            $table->timestamp('activation_code_expires_at')->after('activation_code')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
