e9c059ba47a99699c0c2f29c13f80669e85b269c
mr-leo44 <lionnelkaniki44@gmail.com>
2024-11-21 01:31:33 +0100

    add activate user route

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/activate', [AuthController::class, 'activate'])->name('student.activate');
+    Route::get('/activate/{token}', [UserController::class, 'activate'])->name('user.activate');
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
 });
 
