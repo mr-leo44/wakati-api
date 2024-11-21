30120512a4abccc94ac9e1a1284b901930a3b2b2
mr-leo44 <lionnelkaniki44@gmail.com>
2024-11-21 05:39:23 +0100

    add user register route

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,13 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 
 Route::group(['prefix' => 'auth'], function () {
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/activate', [AuthController::class, 'activate'])->name('student.activate');
-    Route::get('/activate/{token}', [UserController::class, 'activate'])->name('user.activate');
     Route::post('/password/forgot', [AuthController::class, 'sendResetCode'])->name('password.sendReset');
     Route::post('/password/verify', [AuthController::class, 'verifyResetCode'])->name('password.verify');
     Route::post('/password/reset', [AuthController::class, 'resetNewPassword'])->name('password.reset');
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
+    Route::post('/user/register', [UserController::class, 'register'])->name('user.register');
+    Route::get('/activate/{token}', [UserController::class, 'activate'])->name('user.activate');
 });
